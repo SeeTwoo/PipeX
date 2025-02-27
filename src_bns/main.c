e68973878b253067f7cf1c504d2279266602f170
Walter Beschon <wbeschon@paul-f4Ar8s3.clusters.42paris.fr>
2025-02-27 12:33:49 +0100

    fixed some invalid reads

--- src_bns/main.c
+++ src_bns/main.c
@@ -6,7 +6,7 @@
 /*   By: wbeschon <wbeschon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 09:38:10 by wbeschon          #+#    #+#             */
-/*   Updated: 2025/02/26 13:36:54 by wbeschon         ###   ########.fr       */
+/*   Updated: 2025/02/27 12:32:11 by wbeschon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,19 +43,21 @@ int	*get_fds(char **av, int command_number)
 {
 	int		*fds;
 	int		i;
-	char	*infile;
-	char	*outfile;
 
 	fds = malloc(sizeof(int) * (command_number * 2));
 	if (!fds)
 		return (NULL);
-	infile = get_infile(av);
-	outfile = get_outfile(av);
-	fds[0] = open(infile, O_RDONLY);
+	i = 0;
+	while (i < command_number * 2)
+		fds[i++] = -1;
+	fds[0] = open(get_infile(av), O_RDONLY);
 	i = 0;
 	while (i < command_number)
-		pipe(&fds[1 + i++ * 2]);
-	fds[i * 2 + 1] = open(outfile, O_WRONLY | O_TRUNC | O_CREAT);
+	{
+		pipe(&fds[i * 2]);
+		i++;
+	}
+	fds[(i * 2) - 1] = open(get_outfile(av), O_WRONLY | O_TRUNC | O_CREAT);
 	return (fds);
 }
 
@@ -79,24 +81,30 @@ void	print_double_array(char **array)
 	ft_printf("\n");
 }
 
-void	print_int_array(int *array)
+void	print_int_array(int *array, int size)
 {
-	while (*array)
+	int	i;
+
+	i = 0;
+	while (i < size)
 	{
-		ft_printf("%d\n", *array);
-		array++;
+		ft_printf("%d\n", array[i]);
+		i++;
 	}
 	ft_printf("\n");
 }
 
 void	clean(t_args *args)
 {
+	if (!args)
+		return ;
 	if (args->paths)
 		free_double_array(args->paths);
 	if (args->command)
 		free_double_array(args->command);
 	if (args->fds)
 		free(args->fds);
+	free(args);
 }
 
 void	fail(t_args *args, char *msg1, char *msg2)
@@ -110,20 +118,17 @@ void	fail(t_args *args, char *msg1, char *msg2)
 	exit(EXIT_FAILURE);
 }
 
-void	init(t_args *args, int ac, char **av, char **envp)
+void	init(t_args **args, int ac, char **av, char **envp)
 {
-	args = malloc(sizeof(t_args));
+	*args = malloc(sizeof(t_args));
 	if (!args)
-		fail(args, "malloc trouble: ", "try again and hope for the best ");
-	args->command_number = get_command_number(ac, av);
-	args->paths = ft_split(get_path(envp), ":");
-	args->command = NULL;
-	args->fds = get_fds(av, args->command_number);
-	if (!args->fds)
-	{
-		ft_printf("failing\n\n");
-		fail(args, "malloc trouble: ", "try again and hope for the best ");
-	}
+		fail(*args, "malloc trouble: ", "try again and hope for the best\n");
+	(*args)->command_number = get_command_number(ac, av);
+	(*args)->paths = ft_split(get_path(envp), ":");
+	(*args)->command = NULL;
+	(*args)->fds = get_fds(av, (*args)->command_number);
+	if (!(*args)->paths || !(*args)->fds)
+		fail(*args, "init failed: ", "try again and hope for the best\n");
 }
 
 int	main(int ac, char **av, char **envp)
@@ -133,12 +138,11 @@ int	main(int ac, char **av, char **envp)
 	if (ac != 5)
 		return (0);
 	args = NULL;
-	init(args, ac, av, envp);
-	ft_printf("%scommand number is:%s %d\n\n", YELLOW, RESET, args->command_number);
-	ft_printf("%spaths are :%s\n\n", YELLOW, RESET);
+	init(&args, ac, av, envp);
+	ft_printf("%spaths are :%s\n\n", BYELLOW, RESET);
 	print_double_array(args->paths);
-	ft_printf("%sfds are :%s\n\n", YELLOW, RESET);
-	print_int_array(args->fds);
+	ft_printf("%sfds are :%s\n\n", BYELLOW, RESET);
+	print_int_array(args->fds, 4);
 	clean(args);
 	return (0);
 }
